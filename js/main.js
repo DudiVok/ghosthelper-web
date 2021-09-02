16f63fab431694f0ebe70409847dcd49568e2fdd
Bálint Dudás <balix017@gmail.com>
2021-09-02 14:07:15 +0200

    Installed node-modules + Some fixes

--- js/main.js
+++ js/main.js
@@ -4,29 +4,15 @@ $(document).ready(function() {
   $("#resetButton").click(function() {
     $(".form-check-input").each(function(index, elem) {
       $(elem).prop("checked", false);
-      $(elem).show();
+      $(elem).fadeIn("100");
     });
   })
 
   $(".row1switch").click(function() {
-    if($(this).prop("checked"))
-    {
-      $("#cant"+$(this)[0].id.substring(5)).hide();
-    }
-    else
-    {
-      $("#cant"+$(this)[0].id.substring(5)).show();
-    }
+    $("#cant" + $(this)[0].id.substring(5)).fadeToggle(150);
   })
 
   $(".row2switch").click(function() {
-    if($(this).prop("checked"))
-    {
-      $("#found"+$(this)[0].id.substring(4)).hide();
-    }
-    else
-    {
-      $("#found"+$(this)[0].id.substring(4)).show();
-    }
+    $("#found" + $(this)[0].id.substring(4)).fadeToggle(150);
   })
 });
